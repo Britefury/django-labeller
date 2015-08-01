793e78ee6118af840034174111984840953064a5
Geoffrey French <brittix1023@googlemail.com>
2015-08-01 11:55:07 +0100

    Labelling tool size is now a parameter.

--- image_labelling_tool/templatetags/labelling_tool.py
+++ image_labelling_tool/templatetags/labelling_tool.py
@@ -6,9 +6,11 @@ register = template.Library()
 
 
 @register.inclusion_tag('inline/labelling_tool.html')
-def labelling_tool(label_classes, image_ids, initial_image_id, get_image_descriptor_url, update_labels_url):
+def labelling_tool(width, height, label_classes, image_ids, initial_image_id, get_image_descriptor_url, update_labels_url):
     tool_id = uuid.uuid4()
     return {'tool_id': str(tool_id),
+            'width': width,
+            'height': height,
             'label_classes': json.dumps(label_classes),
             'image_ids': json.dumps(image_ids),
             'initial_image_id': str(initial_image_id),

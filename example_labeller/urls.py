40b3e4c86ce192fe98cbe39680d5a3f198995091
Geoffrey French <brittix1023@googlemail.com>
2016-11-28 18:56:14 +0000

    Updated documentation to account for recent changes and improvements. Bug fixes to Django app.

--- example_labeller/urls.py
+++ example_labeller/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'labelling_aas.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -12,4 +12,4 @@ urlpatterns = patterns('',
     url(r'^get_image_descriptor$', views.get_image_desctriptor),
     url(r'^update_labels$', views.set_labels),
     url(r'^image/(?P<image_id>\d+)$', views.get_image),
-)
+]

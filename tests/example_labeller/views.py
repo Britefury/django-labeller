7b5f61c13895ac7d63d53a51056082ce9bde1e54
Geoffrey French <brittix1023@gmail.com>
2020-02-11 15:59:55 +0000

    DEXTR labelling in Django works.

--- tests/example_labeller/views.py
+++ tests/example_labeller/views.py
@@ -1,5 +1,7 @@
 import os, datetime, json
 
+import numpy as np
+
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import ensure_csrf_cookie
 
@@ -36,9 +38,6 @@ def tool(request):
             image_id=img.id, url=img.image.url,
             width=img.image.width, height=img.image.height) for img in models.ImageWithLabels.objects.all()]
 
-    # Convert the label class tuples in `settings` to `labelling_tool.LabelClass` instances
-    label_classes = settings.LABEL_CLASSES
-
     context = {
         'colour_schemes': settings.LABEL_COLOUR_SCHEMES,
         'label_class_groups': [g.to_json() for g in settings.LABEL_CLASSES],
@@ -53,8 +52,39 @@ def tool(request):
 
 
 class LabellingToolAPI (labelling_tool_views.LabellingToolViewWithLocking):
+    def _apply_dextr(self, image, dextr_points_np):
+        if settings.LABELLING_TOOL_DEXTR_WEIGHTS_PATH is not None:
+            if not hasattr(self, '_dextr_model'):
+                import os
+                from dextr.dextr import ResNet101DeepLabDEXTR
+                import torch
+
+                dextr_weights = os.path.expanduser(settings.LABELLING_TOOL_DEXTR_WEIGHTS_PATH)
+
+                dextr_model = ResNet101DeepLabDEXTR()
+                dextr_model.load_weights(dextr_weights)
+
+                device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+                dextr_model.eval()
+                dextr_model.to(device)
+
+                self._dextr_model = dextr_model
+
+            import numpy as np
+            from PIL import Image
+
+            im = np.array(Image.open(image.image.path))
+
+            mask = self._dextr_model.inference(im, dextr_points_np)
+            regions = labelling_tool.PolygonLabel.mask_image_to_regions_cv(mask, sort_decreasing_area=True)
+            regions_js = labelling_tool.PolygonLabel.regions_to_json(regions)
+            return regions_js
+        else:
+            return None
+
+
     def get_labels(self, request, image_id_str, *args, **kwargs):
-        image = get_object_or_404(models.ImageWithLabels, id=image_id_str)
+        image = get_object_or_404(models.ImageWithLabels, id=int(image_id_str))
         return image.labels
 
     def get_next_unlocked_image_id_after(self, request, current_image_id_str, *args, **kwargs):
@@ -70,3 +100,32 @@ class LabellingToolAPI (labelling_tool_views.LabellingToolViewWithLocking):
             return unlocked_img_ids[index]
         else:
             return None
+
+    def dextr_request(self, request, image_id_str, dextr_id, dextr_points):
+        """
+        :param request: HTTP request
+        :param image_id_str: image ID that identifies the image that we are labelling
+        :param dextr_id: an ID number the identifies the DEXTR request
+        :param dextr_points: the 4 points as a list of 2D vectors ({'x': <x>, 'y': <y>}) in the order
+            top edge, left edge, bottom edge, right edge
+        :return: contours/regions a list of lists of 2D vectors, each of which is {'x': <x>, 'y': <y>}
+        """
+        if settings.LABELLING_TOOL_DEXTR_AVAILABLE:
+            image = get_object_or_404(models.ImageWithLabels, id=int(image_id_str))
+            dextr_points = np.array([[p['x'], p['y']] for p in dextr_points])
+            regions_js = self._apply_dextr(image, dextr_points)
+            return regions_js
+        else:
+            return None
+
+    def dextr_poll(self, request):
+        """
+        :param request: HTTP request
+        :return: a list of dicts where each dict takes the form:
+            {
+                'image_id': image ID string that identifies the image that the label applies to
+                'dextr_id': the ID number that identifies the dextr job/request
+                'regions': contours/regions a list of lists of 2D vectors, each of which is {'x': <x>, 'y': <y>}
+            }
+        """
+        return None
